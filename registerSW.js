9d12da770f5fe959ab41ec178e1c3b7f86493464
sanals <5sanal5z5@gmail.com>
2025-05-18 11:16:04 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/amz-affiliate-link/sw.js', { scope: '/amz-affiliate-link/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
